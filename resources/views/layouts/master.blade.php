e66ae71cad1d6562fab634fd239f700e39826efa
univesta-angel <angelica.ucic@gmail.com>
2020-10-27 14:38:51 +0800

    add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,8 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <link rel="icon" type="image/png" href="/images/amoraph_favicon.png"/>
   @yield('styles')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
