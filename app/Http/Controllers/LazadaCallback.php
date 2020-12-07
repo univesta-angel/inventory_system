dc74b3026fed4695a8244fe42bd8cde560ba4fb3
Angelica May F. Santiago <frondozoangel@gmail.com>
2020-12-07 13:45:14 +0800

    add callback url for lazada

---
+++ app/Http/Controllers/LazadaCallback.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Audit;
+
+use Illuminate\Support\Facades\Auth;
+
+
+class LazadaCallback extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
+    public function show()
+    {
+        $user = Auth::user();
+        );
+    }
+
+    public function callback(){
+        echo 'abc';
+    }
+}
