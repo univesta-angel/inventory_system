c2104e497c6dd3607765d6421336596798de1e93
frondozoangel@gmail.com <angelica.ucic@gmail.com>
2020-12-09 11:59:34 +0800

    save code and access token to database on authorization complete

--- app/Http/Controllers/LazadaCallback.php
+++ app/Http/Controllers/LazadaCallback.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Audit;
+use App\Models\EcomPlatform;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
+
+use GuzzleHttp\Client;
 
 class LazadaCallback extends Controller
 {
@@ -21,12 +28,66 @@ class LazadaCallback extends Controller
     public function show()
     {
         $user = Auth::user();
-        echo 'Authorization success!';
-        die();
     }
 
     public function callback(Request $request){
-        var_dump($request->all());
+        $arr = $request->all();
+        $code = $arr['code'];
+
+        DB::table('lazada_auth')->insert([
+            'code' => Crypt::encryptString($code)
+        ]);
+
+        $platform = EcomPlatform::find(2);
+        if ($platform->api_key != '') {
+            $appkey = Crypt::decryptString($platform->api_key);
+        }
+        else {
+            echo 'Missing API Key.<br>';
+        }
+        if ($platform->password != '') {
+            $appSecret = Crypt::decryptString($platform->password);
+        } else {
+            echo 'Missing Password.<br>';
+        }
+
+        /*$c = new LazopClient($url,$appkey,$appSecret);
+        $request = new LazopRequest('/auth/token/create');
+        $request->addApiParam('code','0_124157_dxtu3JzMDqZMFTpbEeKD7p0T34358');
+
+        $result = $c->execute($request);
+        $result_json = json_decode($result);*/
+
+        $data = array(
+            "code" => $code,
+        );
+
+        $client = new \GuzzleHttp\Client();
+        $base_url = "https://api.lazada.com.ph/rest";
+        $parameters = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ]
+        ];
+        if(!empty($data)){ $parameters['json'] = $data;}
+        $response = $client->request($method, $base_url,$parameters);
+        $body = $response->getBody();
+        $stringBody = (string) $body;
+        $json = json_decode($body);
+
+        DB::table('lazada_auth')->insert([
+            'access_token' => $json->access_token,
+            'refresh_token' => $json->refresh_token,
+            'country' => $json->country,
+            'refresh_expires_in' => $json->refresh_expires_in,
+            'account_platform' => $json->account_platform,
+            'expires_in' => $json->expires_in,
+            'account' => $json->account,
+            'country_user_info' => json_encode($json->country)
+        ]);
+
+        echo 'Authorization success and access token saved!';
         die();
     }
 }
